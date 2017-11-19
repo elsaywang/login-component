dbafeeb1d5eaad3da61291217c44ab6d4ae98f76
Elsa Wang <elsawang1230@gmail.com>
2017-11-18 22:23:42 -0500

    add ts tsx for exercise

--- configs/webpack.common.js
+++ configs/webpack.common.js
@@ -4,18 +4,19 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 const SRC_DIR = path.join(__dirname, '..', 'src'),
 	SASS_DIR = path.join(__dirname, '..', 'styles'),
 	DIST_DIR = path.join(__dirname, '..', 'dist');
-
 module.exports = {
 	entry: [path.join(SRC_DIR, "index.js")],
 	output: {
 		path: DIST_DIR,
-		filename: "bundle.js"
+		filename: "bundle.js",
 	},
 	module: {
 		rules: [
 			{
 				test: /\.css$/,
-				use: ['style-loader', 'css-loader',]
+				use: [
+					'style-loader', 'css-loader',
+				],
 			}, {
 				test: /\.scss$/,
 				include: SASS_DIR,
@@ -27,20 +28,24 @@ module.exports = {
 					}, {
 						loader: 'sass-loader'
 					},
-				]
+				],
 			}, {
 				test: /bootstrap.+\.(jsx|js)$/,
-				loader: 'imports-loader?jQuery=jquery,$=jquery,this=>window'
+				loader: 'imports-loader?jQuery=jquery,$=jquery,this=>window',
 			}, {
 				test: /\.js$|\.jsx$/,
 				include: SRC_DIR,
 				loader: 'babel-loader',
 				options: {
 					presets: ['react', 'es2015', 'stage-0',]
-				}
+				},
+			}, {
+				test: /\.(ts|tsx)$/,
+				include: SRC_DIR,
+				loader: 'awesome-typescript-loader',
 			}, {
 				test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-				loader: 'file-loader?name=/[path][name].[ext]'
+				loader: 'file-loader?name=/[path][name].[ext]',
 			},
 		]
 	},
@@ -49,7 +54,7 @@ module.exports = {
 		new HTMLWebpackPlugin({
 			filename: 'index.html',
 			template: path.join(SRC_DIR, 'index.html'),
-			inject: false
+			inject: false,
 		}),
 		new webpack.DefinePlugin({
 			'process.env': {
@@ -62,6 +67,8 @@ module.exports = {
 		alias: {
 			"Config": path.join(__dirname, "api.config")
 		},
-		extensions: ['.js', '.jsx',]
-	}
+		extensions: [
+			'.js', '.jsx',
+		],
+	},
 };
